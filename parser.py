36d0c1d9a0c247a3c446b9c13d15df6532ed70d7
satrancci <thescarface1993@gmail.com>
2021-02-17 23:32:30 -0600

    removed unnecessary lines

--- parser.py
+++ parser.py
@@ -1,6 +1,5 @@
 from bs4 import BeautifulSoup
 
-TEST_PAGE = "1362215.html"
 
 class Parser:
    def __init__(self, f):
@@ -11,7 +10,3 @@ class Parser:
         return (True, price.get("content")) if price else (False, 0)
 
 
-if __name__ == "__main__":
-   with open(TEST_PAGE) as f:
-    parser = Parser(f)
-    print(parser.parse())
