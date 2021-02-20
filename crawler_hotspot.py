2d7a9a336c5d1b4c480c31e9c4eaf5926513ad55
satrancci <thescarface1993@gmail.com>
2021-02-19 23:16:01 -0600

    removed crawl_with_hotspot_shield()

--- crawler_hotspot.py
+++ crawler_hotspot.py
@@ -84,31 +84,6 @@ def hotspot_status(sleep_time=10):
     return ret_val
     
 
-def crawl_with_hotspot_shield(base_url, route_id, base_dir):
-
-    command = f"curl '{base_url}/{route_id}' \
-    --header 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'\
-    -b cookies.txt -c cookies.txt \
-    --dump-header {base_dir}/{route_id}_header.txt \
-    > {base_dir}/{route_id}.html"
-
-    err_file = open(f"{base_dir}/{route_id}_logs.txt", 'w')
-    out_file = open(base_dir+f"/{route_id}.html", 'w')
-
-    try:
-        out = subprocess.check_output(command, shell=True, timeout=20, stderr=err_file)
-        #out = out.decode('utf-8')
-        #out_file.write(out)
-
-    except subprocess.CalledProcessError as exc:
-        print(f"Status: FAILED, code: {exc.returncode}")
-        raise
-    finally:
-        err_file.close()
-        out_file.close()
-
-
-
 
 if __name__=='__main__':
     codes = import_hotspot_codes("hotspot_shield_codes.txt")
