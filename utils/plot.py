01208ad8d5e48103baf77a7baabb54bdef497231
satrancci <thescarface1993@gmail.com>
2021-02-21 00:34:27 -0600

    minor improvement to plots

--- utils/plot.py
+++ utils/plot.py
@@ -12,7 +12,7 @@ def plot_single_cdf(city, prices):
     mean, std = np.mean(prices), np.std(prices)
     cdf = norm.cdf(prices, mean, std)
     plt.clf()
-    plt.plot(prices,cdf,label=f"{city}-cdf")
+    plt.plot(prices,cdf,label=f"{city} (n={len(prices)})")
     plt.xlabel("Prices")
     plt.ylabel("Probability")
     plt.title("CDF for continuous distribution")
@@ -25,16 +25,16 @@ def plot_multiple_cdf(cities_to_prices):
     '''
     cities_to_prices: defaultdict(list) with CITY as key and PRICES as value.
     '''
-
+    N = sum(map(lambda x: len(x), cities_to_prices.values()))
     plt.xlabel("Prices")
     plt.ylabel("Probability")
-    plt.title("CDF for continuous distribution")
+    plt.title(f"CDF for continuous distribution (N={N})")
 
     for city, prices in cities_to_prices.items():
         prices = sorted(prices)
         mean, std = np.mean(prices), np.std(prices)
         cdf = norm.cdf(prices, mean, std)
-        plt.plot(prices,cdf,label=f"{city}-cdf")
+        plt.plot(prices,cdf,label=f"{city} (n={len(prices)})")
         plt.legend()
 
 
