7c1c08d201d2e7bf3dabe08471c1e8d67967a170
satrancci <thescarface1993@gmail.com>
2021-02-21 16:08:04 -0600

    update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 python3 -m pip install --upgrade pip
-pip3 install virtualenv
+python3 -m pip install --user virtualenv
+python3 -m venv env
+source env/bin/activate && python3 -m pip install -r requirements.txt
+mkdir -p crawled_routes && mkdir -p routes_to_crawl && mkdir -p fetched_routes && mkdir -p plots
 source env/bin/activate
-python3 -m pip install -r requirements.txt
-
-mkdir -p crawled_routes
-mkdir -p routes_to_crawl
-mkdir -p fetched_routes
-mkdir -p plots
