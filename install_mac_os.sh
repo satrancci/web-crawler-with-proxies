4f001867691abe047f4724ce820ca4c764f9e1cc
satrancci <thescarface1993@gmail.com>
2021-02-21 22:17:13 -0600

    add installation commands for Mac OS

---
+++ install_mac_os.sh
@@ -0,0 +1,37 @@
+########## UNCOMMENT LINES WITH ONE '#' TO RUN THE COMMANDS ##########
+
+##### Install virtualenv and Python packages #####
+bash install.sh
+
+##### Install Chrome Driver for Mac OS #####
+##### Steps based on: https://stackoverflow.com/questions/38081021/using-selenium-on-mac-chrome #####
+
+#cd $HOME/Downloads
+#wget http://chromedriver.storage.googleapis.com/2.41/chromedriver_mac64.zip
+#unzip chromedriver_mac64.zip
+#mkdir -p $HOME/local/bin
+#mv chromedriver $HOME/local/bin
+#echo "export PATH=$PATH:$HOME/local/bin" >> $HOME/.bash_profile
+
+##### Then run #####
+# which chromedriver
+##### and copy paste that path into run_selenium.py on the line starting with
+#####  driver=webdriver.Chrome(PASTE_THE_PATH_HERE, options=options) #####
+
+##### Install Crawlera certificate ######
+
+##### Download certificate manually from https://support.zyte.com/support/solutions/articles/22000188407-fetching-https-pages-with-zyte-smart-proxy-manager
+
+##### Copy the downloaded certificate file to /usr/local/share/ca-certificates/directory to make it implicitly trusted #####
+# sudo cp zyte-proxy-ca.crt /usr/local/share/ca-certificates/zyte-proxy-ca.crt
+
+##### Update stored Certificate Authority files ######
+# sudo update-ca-certificates
+
+
+
+
+
+
+
+
